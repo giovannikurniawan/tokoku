3fff5da5dafef814c9ad2c950d7a6d028b7a4aec
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-08-01 07:07:30 +0700

    update helpers

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -11,16 +11,22 @@ function terbilang($angka)
     $baca = array('', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas');
     $terbilang = '';
 
-    if ($angka < 12) {
+    if ($angka < 12) { // 1 - 11
         $terbilang = ' ' . $baca[$angka];
-    } elseif ($angka < 20) {
+    } elseif ($angka < 20) { // 12 - 19
         $terbilang = terbilang($angka - 10) . ' belas';
-    } elseif ($angka < 100) {
+    } elseif ($angka < 100) { // 20 - 99
         $terbilang = terbilang($angka / 10) . ' puluh' . terbilang($angka % 10);
-    } elseif ($angka < 200) {
+    } elseif ($angka < 200) { // 100 - 199
         $terbilang = ' seratus' . terbilang($angka - 100);
-    } elseif ($angka < 1000) {
+    } elseif ($angka < 1000) { // 200 - 999
         $terbilang = terbilang($angka / 100) . ' ratus' . terbilang($angka % 100);
+    } elseif ($angka < 2000) { // 1000 - 1999
+        $terbilang = ' seribu' . terbilang($angka - 1000);
+    } elseif ($angka < 1000000) { // 2000 - 999999
+        $terbilang = terbilang($angka / 1000) . 'ribu' . terbilang($angka % 1000);
+    } elseif ($angka < 1000000000) { // 1000000 - 999.999.990
+        $terbilang = terbilang($angka / 1000000) . ' juta' . terbilang($angka % 1000000);
     }
 
     return $terbilang;
