c3af552df838cbdf5f07bcf0271624680284a968
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-08-01 14:38:19 +0700

    helpers

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -31,3 +31,31 @@ function terbilang($angka)
 
     return $terbilang;
 }
+
+function tanggal_indonesia($tgl, $tampil_hari = true)
+{
+
+    $nama_hari = array(
+        'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'
+    );
+
+    $nama_bulan = array(
+        1 =>
+        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    );
+
+    $tahun = substr($tgl, 0, 4);
+    $bulan = $nama_bulan[(int)substr($tgl, 5, 2)];
+    $tanggal = substr($tgl, 8, 2);
+    $text = '';
+
+    if ($tampil_hari) {
+        $urutan_hari = date('w', mktime(0, 0, 0, substr($tgl, 5, 2), $tanggal, $tahun));
+        $hari        = $nama_hari[$urutan_hari];
+        $text        .= "$hari, $tanggal $bulan $tahun";
+    } else {
+        $text        .= "$tanggal $bulan $tahun";
+    }
+
+    return $text;
+}
